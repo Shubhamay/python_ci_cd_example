ac32222fe14fc1e5da8628bc669cf14c2c719940
shubham16121988@gmail.com <Anusua_1>
2020-08-28 08:46:31 +0530

    added bootstrap and layout and other html page

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Anusua Dey',
+        'title' : 'Blog Post'
+    },
+    {
+        'author' : 'Shubhamay Das',
+        'title' : 'Blog Post'
+    }
+]
+
+# Decorator
 @app.route("/")
-def index():
-    return "Hello World"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
